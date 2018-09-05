52e25fb168f218db18416c39a989911a65a7b429
Tamur Farrokhnia <tamur@Tamurs-MacBook-Pro.local>
2018-09-05 17:11:14 -0500

    add html, css, and images for pages

---
+++ week 2/FirstWebApp/src/com/tamurfarrokhnia/FirstServlet.java
@@ -0,0 +1,13 @@
+package com.tamurfarrokhnia;
+
+import java.io.IOException;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
